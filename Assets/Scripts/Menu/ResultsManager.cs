72ba88e97e346dd36ae622c64ccf4fa8a77c3a62
Max Brogno <mgbrogno@gmail.com>
2019-05-22 14:54:02 +1000

    Added Menu Scripts

---
+++ Assets/Scripts/Menu/ResultsManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ResultsManager : MonoBehaviour {
+
+    public float playerScore;
+    public float highScore;
+
+    public GameObject scoreText;
+    public GameObject highscoreText;
+
+    public Button restartButton;
+    public Button menuButton;
+    public Button exitButton;
+
+    void Start () {
+        restartButton.GetComponent<Button>().onClick.AddListener(PressRestart);
+        menuButton.GetComponent<Button>().onClick.AddListener(PressMenu);
+        exitButton.GetComponent<Button>().onClick.AddListener(PressExit);
+
+        playerScore = PlayerPrefs.GetFloat("score");
+        highScore = PlayerPrefs.GetFloat("highscore");
+
+        if (playerScore > highScore) {
+            PlayerPrefs.SetFloat("highscore", playerScore);
+            highScore = playerScore;
+        }
+
+        scoreText.GetComponent<TextMeshProUGUI>().text = "Time: " + playerScore;
+        highscoreText.GetComponent<TextMeshProUGUI>().text = "Highscore: " + highScore;
+    }
+
+    void PressRestart() {
+        SceneManager.LoadScene(1);
+    }
+
+    void PressMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+    void PressExit() {
+        Application.Quit();
+    }
+
+}
