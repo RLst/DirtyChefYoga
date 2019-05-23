0ffb40dc621cc660b883b01b294cd224c8685df0
maxbrogno <mgbrogno@gmail.com>
2019-05-23 14:11:15 +1000

    Added Credits scene to menu.

--- Assets/Scripts/Max/Menu/MenuManager.cs
+++ Assets/Scripts/Max/Menu/MenuManager.cs
@@ -9,14 +9,17 @@ public class MenuManager : MonoBehaviour {
     // Main Buttons
     public Button playButton;
     public Button controlsButton;
+    public Button creditsButton;
     public Button exitButton;
 
     // UI Panels
     public GameObject menuPanel;
     public GameObject controlsPanel;
+    public GameObject creditsPanel;
 
     // Back Buttons
     public Button backButton;
+    public Button backCreditsButton;
 
     // View State
     CurrentView currentView;
@@ -24,6 +27,7 @@ public class MenuManager : MonoBehaviour {
 
         MenuView,
         ControlsView,
+        CreditsView
 
     }
 
@@ -32,10 +36,12 @@ public class MenuManager : MonoBehaviour {
         // Assign buttons to methods
         playButton.GetComponent<Button>().onClick.AddListener(PressPlay);
         controlsButton.GetComponent<Button>().onClick.AddListener(PressControls);
+        creditsButton.GetComponent<Button>().onClick.AddListener(PressCredits);
         exitButton.GetComponent<Button>().onClick.AddListener(PressExit);
 
         backButton.GetComponent<Button>().onClick.AddListener(PressBack);
-        
+        backCreditsButton.GetComponent<Button>().onClick.AddListener(PressBack);
+
         // Set the view to the main menu
         currentView = CurrentView.MenuView;
     }
@@ -47,16 +53,25 @@ public class MenuManager : MonoBehaviour {
             case CurrentView.MenuView:
                 menuPanel.SetActive(true);
                 controlsPanel.SetActive(false);
+                creditsPanel.SetActive(false);
                 break;
 
             case CurrentView.ControlsView:
                 menuPanel.SetActive(false);
                 controlsPanel.SetActive(true);
+                creditsPanel.SetActive(false);
+                break;
+
+            case CurrentView.CreditsView:
+                menuPanel.SetActive(false);
+                controlsPanel.SetActive(false);
+                creditsPanel.SetActive(true);
                 break;
 
             default:
                 menuPanel.SetActive(true);
                 controlsPanel.SetActive(false);
+                creditsPanel.SetActive(false);
                 break;
         }
 
@@ -68,6 +83,10 @@ public class MenuManager : MonoBehaviour {
 
     void PressControls() {
         currentView = CurrentView.ControlsView;
+    }
+
+    void PressCredits() {
+        currentView = CurrentView.CreditsView;
     }
 
     void PressExit() {
