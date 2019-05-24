b109958641c2b8b4d73fe5f986e5dc35b0b045ad
Tony Le <letony@icloud.com>
2019-05-24 10:02:54 +1000

    Added auto disposer

--- Assets/Scripts/Station/AutoDisposeFood.cs
+++ Assets/Scripts/Station/AutoDisposeFood.cs
@@ -2,8 +2,15 @@
 
 namespace DirtyChefYoga
 {
+    [RequireComponent(typeof(Collider))]
     public class AutoDisposeFood : MonoBehaviour
     {
+        Collider col;
+
+        void Start()
+        {
+            col = GetComponent<Collider>();
+        }
         void OnCollisionEnter(Collision col)
         {
             var hit = col.collider.GetComponent<Ingredient>();
@@ -11,6 +18,12 @@ namespace DirtyChefYoga
             {
                 Destroy(hit.gameObject);
             }
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(transform.position, new Vector3(0.3f, 3, 0.3f));
         }
     }
 }
