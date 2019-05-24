4a32c3c1b6c9e8d0006b3c5c38f296bc2b85cc8a
Tony Le <letony@icloud.com>
2019-05-25 03:58:36 +1000

    Added Particle Alert script

---
+++ Assets/Scripts/Utils/ParticleAlert.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+namespace DirtyChefYoga.Util
+{
+	//Just provides an easy way to create a visual alert
+	public class ParticleAlert : MonoBehaviour
+	{
+		[SerializeField] ParticleSystem particle;
+		
+		public void Activate(float duration)
+		{
+			particle.Play();
+			StartCoroutine(timer(duration));
+
+			IEnumerator timer(float delay)
+			{
+				while (true)
+				{
+					yield return new WaitForSeconds(delay);
+					particle.Pause();
+					break;
+				}
+			}
+		}
+	}
+}
