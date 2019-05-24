7277a2120f17e7db8fd448fba004b7930e171ceb
Tony Le <letony@icloud.com>
2019-05-24 13:25:32 +1000

    Added draw gizmo utility

---
+++ Assets/Scripts/Utils/DrawGizmo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace DirtyChefYoga.Util
+{
+    public enum GizmoType
+    {
+        Sphere,
+        Box
+    }
+
+    public class DrawGizmo : MonoBehaviour
+    {
+        [SerializeField] GizmoType type = GizmoType.Box;
+        [SerializeField] Color color = new Color(1, 0.5f, 0, 0.75f);
+        [SerializeField] float radius = 1f;
+        [SerializeField] Vector3 size = new Vector3(1,1,1);
+        [SerializeField] Vector3 offset = new Vector3();
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = color;
+            switch (type)
+            {
+                case GizmoType.Sphere:
+                    Gizmos.DrawWireSphere(transform.position + offset, radius);
+                    break;
+                case GizmoType.Box:
+                    Gizmos.DrawWireCube(transform.position + offset, size);
+                    break;
+            }
+        }
+    }
+}
