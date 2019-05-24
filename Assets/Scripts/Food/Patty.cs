9db648e476fcee6f0a191c58b4b3ace0a2ee2e5e
Tony Le <letony@icloud.com>
2019-05-25 03:37:59 +1000

    Patty cooking shader works

--- Assets/Scripts/Food/Patty.cs
+++ Assets/Scripts/Food/Patty.cs
@@ -1,7 +1,25 @@
 ﻿using UnityEngine;
 namespace DirtyChefYoga
 {
-    public class Patty : BurgerIngredient
-    {
-    }
+	public class Patty : BurgerIngredient
+	{
+		Material mat;
+		public override float cookProgress
+		{
+			set
+			{
+				//Set as usual
+				m_cookProgress = value;
+				if (m_cookProgress > 2f) m_cookProgress = 2f;
+
+				//Also set the shader's burn value
+				mat.SetFloat("_CookAmount", m_cookProgress);
+			}
+		}
+		private void Start()
+		{
+			mat = GetComponentInChildren<MeshRenderer>().material;
+		}
+	}
+
 }
