bf61b618769422efb5af1473a1e165746a7f2501
Tony Le <letony@icloud.com>
2019-05-24 16:42:43 +1000

    Benchtop and basic cooking stations working

---
+++ Assets/Scripts/Station/Benchtop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+namespace DirtyChefYoga
+{
+    public class Benchtop : Station
+    {
+        public override bool Interact(Ingredient ingredient)
+        {
+            OnInteract.Invoke();
+            
+            //Place on the work surface
+            ingredient.transform.position = workSurface.position;
+
+            //Turn off physics
+            ingredient.SetPhysicsActive(false);
+
+            return true;
+        }
+    }
+}
--- Assets/Scripts/Station/CookingStation.cs
+++ Assets/Scripts/Station/CookingStation.cs
@@ -1,13 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 namespace DirtyChefYoga
 {
-    public class CookingStation : Station
+    public abstract class CookingStation : Station
     {
+        [SerializeField] float cookAmount = 0.001f;
+        // [SerializeField] float cookTime = 5f;
+        [SerializeField] UnityEvent OnStartCooking;
+        public bool isCooking
+        {
+            get
+            {
+                return currentIngredientCooking != null;
+                // return workingSuface.childCount > 0;
+            }
+        }
+        Ingredient currentIngredientCooking;
+
+        protected void Update()
+        {
+            HandleCooking();
+        }
+
         public override bool Interact(Ingredient ingredient)
         {
-            throw new System.NotImplementedException();
+            OnInteract.Invoke();
+            
+            if (isCooking)
+            {
+                Debug.Log("Already cooking!");
+                return false;
+            }
+            if (!ingredient.isCookable)
+            {
+                Debug.Log("Can't cook this item!");
+                return false;
+            }
+
+            ////COOK!
+            Debug.Log("Started cooking item!");
+
+            //1. Place item on surface and stop physics
+            ingredient.transform.position = workSurface.position;
+            ingredient.SetPhysicsActive(false);
+
+            //2. Start cooking
+            currentIngredientCooking = ingredient;
+            OnStartCooking.Invoke();
+
+            return true;
+        }
+
+        protected virtual void HandleCooking()
+        {
+            //Continue cooking any ingredients
+            if (!currentIngredientCooking) return;
+
+            // Debug.Log("Cooking " + currentIngredientCooking);
+            // Debug.Log("Progress " + currentIngredientCooking.cookProgress);
+            // var cookAmount = cookTime * 0.1f * Time.deltaTime;
+            // Debug.Log("Cook amount: " + cookAmount);
+
+            currentIngredientCooking.cookProgress += cookAmount;
         }
     }
 }
