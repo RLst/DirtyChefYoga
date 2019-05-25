c0e8ce994a50aefeddc21322bba0cf2d81992656
Tony Le <letony@icloud.com>
2019-05-25 21:40:12 +1000

    Separated out interaction logic from player movement controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ namespace DirtyChefYoga
         [SerializeField] bool debug = true;
         [Space]
 
+
         [Header("Move")]
         [SerializeField] float maxSpeed = 5f;
 
@@ -22,63 +23,24 @@ namespace DirtyChefYoga
         private bool isDashing;
         float currentDashSpeed;
 
-        [Header("Interaction")]
-		// [SerializeField] new Collider collider;
-        [SerializeField] Transform handPoint;
-        [SerializeField] Vector3 castHalfExtents = new Vector3(0.3f, 1, 0.5f);
-		[SerializeField] float castLength = 1.5f;
-
-
-        public bool isHoldingItem
-        {
-            get {
-                //If there is something childed to this transform
-                //means we're holding something
-                return currentItem != null;
-                // return handPoint.childCount > 0;       
-            }
-        }
-
-        public UnityEvent OnInvalidAction;
-
-        Ingredient currentItem;
-
         private CharacterController controller;
         private PlayerInput input;
         Camera cam;
         private Quaternion facing;
 
-        // private PlayerInteract interact;
+
         void Start()
         {
             controller = GetComponent<CharacterController>();
             input = GetComponent<PlayerInput>();
-            // interact = GetComponent<PlayerInteract>();
             cam = FindObjectOfType<Camera>();
         }
 
         void Update()
         {
             MoveCharacter();
-            if (input.interacted) HandleInteractions();
-
-			db();
         }
 
-		private void db()
-		{
-			if (DetectInteractable<Ingredient>(out Ingredient ingredient))
-			{
-				Debug.Log("Detecting ingredient!: " + ingredient);
-			}
-			else if (DetectInteractable<Station>(out Station station))
-			{
-				Debug.Log("Detecting station!: " + station);
-			}
-
-		}
-
-		#region MOVE
 		void MoveCharacter()
         {
             #region Move & Dash
@@ -123,146 +85,16 @@ namespace DirtyChefYoga
             controller.Move(resultantMotion * Time.deltaTime);
             transform.rotation = facing;
         }
-        #endregion
-
-        #region INTERACTIONS
-        private void HandleInteractions()
-        {
-            //If holding ingredient
-            if (currentItem)
-            {
-                //If there's a station in front of you then interact with it using the ingredient
-                if (DetectInteractable<Station>(out Station stationHit))
-                {
-                    // Debug.Log("Station found");
-
-                    //Pass ingredient to station
-                    if (stationHit.Interact(currentItem))
-                    {
-                        Debug.Log("Successfully passed ingredient to station");
-                        ReleaseIngredient();
-                    }
-                    OnInvalidAction.Invoke();
-                }
-                //Otherwise drop the ingredient
-                else
-                {
-                    ReleaseIngredient();
-                }
-            }
-            //If not holding ingredient
-            else
-            {
-                //If ingredient found
-                if (DetectInteractable<Ingredient>(out Ingredient ingredientHit))
-                {
-                    // Debug.Log("Ingredient found");
-
-                    PickUpIngredient(ingredientHit);
-                }
-                //Do nothing if you're not holding anything
-            }
-        }
-
-
-
-        private void ReleaseIngredient()
-        {
-            Debug.Log("Drop the item!");
-
-            //Unchild
-            currentItem.transform.SetParent(null);
-
-            //Make it a physics object
-            currentItem.SetPhysicsActive(true);
-
-            //RELEASE
-            currentItem = null;
-        }
-
-        private void PickUpIngredient(Ingredient ingredientHit)
-        {
-            Debug.Log("Picking up ingredient!");
-
-            ////Pick it up
-            //Set as picked up
-            currentItem = ingredientHit;
-            //Move to the hand
-            currentItem.transform.SetPositionAndRotation(handPoint.transform.position, handPoint.transform.rotation);
-            //Set it as a child
-            currentItem.transform.SetParent(this.transform);
-            //Deactivate physics
-            currentItem.SetPhysicsActive(false);
-        }
-
-
-        //Detect object of type T according to set cast paramters
-        bool DetectInteractable<T>(out T hit) where T : MonoBehaviour
-        {
-
-			bool isHit = Physics.BoxCast(transform.position, castHalfExtents, transform.forward, out RaycastHit hitInfo, Quaternion.LookRotation(transform.forward), castLength);
-            // bool isHit = Physics.BoxCast(transform.position + castOffset, castHalfExtents, transform.forward, out RaycastHit castHit, transform.rotation);
-
-            //If something hit
-            if (isHit)
-            {
-				// Debug.DrawRay(transform.position, transform.forward * 10, Color.blue);
-                hit = hitInfo.collider.GetComponent<T>();
-                //Get found component
-                if (hit is T) //item is the right type!
-                {
-					DrawDebugLineArray(0.25f, Color.green);
-                    //set found object
-                    return true;
-                }
-            }
-            //Nothing found
-            hit = null;
-            return false;
-        }
-        #endregion
-
 
         void OnGUI()
         {
             if (debug)
             {
                 GUILayout.Label("Player Controller");
-                if (currentItem != null)
-                {
-                    GUILayout.Label("Holding a " + currentItem.name);
-                }
+				GUILayout.Space(5);
+                GUILayout.Label("isDashing: " + isDashing);
             }
         }
-
-		void OnDrawGizmos() 
-		{
-			Gizmos.color = Color.red;
-			Transform t = transform;
-			for (float i = -castHalfExtents.y; i < castHalfExtents.y; i += 0.25f)
-			{
-				Vector3 leftFrom = t.position + t.up * i - t.right * castHalfExtents.x;
-				Vector3 rightFrom = t.position + t.up * i + t.right * castHalfExtents.x;
-				Vector3 leftTo = t.position + t.up * i - t.right * castHalfExtents.x + t.forward * castLength;
-				Vector3 rightTo = t.position + t.up * i + t.right * castHalfExtents.x + t.forward * castLength;
-				Gizmos.DrawLine(leftFrom, leftTo);
-				Gizmos.DrawLine(rightFrom, rightTo);
-			}
-		}
-
-		void DrawDebugLineArray(float arraySpacing, Color color)
-		{
-			for (float i = -castHalfExtents.y; i < castHalfExtents.y; i += arraySpacing)
-			{
-				for (float j = -castHalfExtents.x; j < castHalfExtents.x; j += arraySpacing)
-				{
-					var t = transform;
-					var from = t.position + t.up*i + t.right*j;
-					var to = t.position + t.up*i + t.right*j + t.forward*castLength;
-					Debug.DrawLine(from, to, color);
-				}
-			}
-		}
 
     }
 }
--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -1,46 +1,192 @@
 ﻿using UnityEngine.Assertions;
 using UnityEngine;
+using System;
+
 namespace DirtyChefYoga
 {
-    //Handles player interactions
-    public class PlayerInteract : MonoBehaviour
-    {
-        [SerializeField] Transform hand;
-        [SerializeField] Vector3 castExtents = new Vector3(0.3f, 1, 0.5f);
-        [SerializeField] float castLength;
+	//Handles player interactions
+	public class PlayerInteract : MonoBehaviour
+	{
+		[SerializeField] bool debug = true;
+		[Space]
 
-        Ingredient itemHeld;
+		[SerializeField] Transform anchor;
+		[SerializeField] Vector3 castHalfExtents = new Vector3(0.45f, 2, 0.2f);
+		[SerializeField] float castLength = 2.4f;
 
-        void Start()
-        {
-            Assert.IsNotNull(hand, "No hand transform found!");
-        }
+		public bool isHoldingItem
+        	{ get { return currentItem != null; } }
 
-        public void PickUpIngredient(Ingredient Ingredient)
-        {
+		PlayerInput input;
+		Ingredient currentItem;
 
-        }
+		void Start()
+		{
+			Assert.IsNotNull(anchor, "No hand transform found!");
+			input = GetComponent<PlayerInput>();
+		}
 
-        //Detect object of type T according to set cast paramters
-        public bool DetectInteractable<T>(out T interactableFound) where T : MonoBehaviour
-        {
-            bool isHit = Physics.BoxCast(transform.position, castExtents, transform.forward, out RaycastHit hit, transform.rotation, castLength);
-            T hitComponent = hit.collider.GetComponent<T>();
+		void Update()
+		{
+			HandleInteractions();
+			// db();
+		}
 
-            //If something hit
-            if (isHit)
-            {
-                //Get found component
-                if (hitComponent is T) //item is the right type!
-                {
-                    //set found object
-                    interactableFound = hitComponent;
-                    return true;
-                }
-            }
-            //Nothing found
-            interactableFound = null;
-            return false;
-        }
-    }
+		private void HandleInteractions()
+		{
+			if (input.interacted)
+			{
+				//If holding ingredient
+				if (isHoldingItem)
+				{
+					//If there's a station in front of you then interact with it using the ingredient
+					if (DetectInteractable<Station>(out Station stationHit))
+					{
+						Debug.Log("Station found");
+						//Pass ingredient to station
+						if (stationHit.Insert(currentItem))
+						{
+							Debug.Log("Successfully passed ingredient to station");
+							ReleaseIngredient();
+						}
+					}
+					//Otherwise drop the ingredient
+					else
+					{
+						ReleaseIngredient();
+					}
+				}
+				//If not holding ingredient
+				else
+				{
+					
+
+					//If a station is found
+					// if (DetectInteractable<Station>(out Station station))
+					// {
+					// 	//Remove ingredient instead of interacting with it
+					// 	if (station.Remove(out Ingredient item))
+					// 	{
+					// 	}
+					// }
+
+					//If ingredient found
+					if (DetectInteractable<Ingredient>(out Ingredient hit))
+					{
+						PickUpIngredient(hit);
+					}
+					//Do nothing if you're not holding anything
+				}
+
+			}
+		}
+
+		private void ReleaseIngredient()
+		{
+			Debug.Log("Drop the item!");
+
+			
+
+			// currentItem
+
+			//Unchild
+			currentItem.transform.SetParent(null);
+			//Make it a physics object
+			currentItem.SetPhysicsActive(true);
+			//RELEASE
+			currentItem = null;
+		}
+
+		private void PickUpIngredient(Ingredient ingredientHit)
+		{
+			Debug.Log("Picking up ingredient!");
+
+			////Pick it up
+			//Set as picked up
+			currentItem = ingredientHit;
+			//Move to the hand
+			currentItem.transform.SetPositionAndRotation(anchor.transform.position, anchor.transform.rotation);
+			//Set it as a child
+			currentItem.transform.SetParent(this.transform);
+			//Deactivate physics
+			currentItem.SetPhysicsActive(false);
+		}
+
+		//Detect object of type T according to set cast paramters
+		bool DetectInteractable<T>(out T hit) where T : MonoBehaviour
+		{
+			bool isHit = Physics.BoxCast(transform.position, castHalfExtents, transform.forward, out RaycastHit hitInfo, Quaternion.LookRotation(transform.forward), castLength);
+
+			//If something hit
+			if (isHit)
+			{
+				// Debug.DrawRay(transform.position, transform.forward * 10, Color.blue);
+				hit = hitInfo.collider.GetComponent<T>();
+				//Get found component
+				if (hit is T) //item is the right type!
+				{
+					DrawDebugLineArray(0.25f, Color.green);
+					//set found object
+					return true;
+				}
+			}
+			//Nothing found
+			hit = null;
+			return false;
+		}
+
+		void DrawDebugLineArray(float arraySpacing, Color color)
+		{
+			for (float i = -castHalfExtents.y; i < castHalfExtents.y; i += arraySpacing)
+			{
+				for (float j = -castHalfExtents.x; j < castHalfExtents.x; j += arraySpacing)
+				{
+					var t = transform;
+					var from = t.position + t.up*i + t.right*j;
+					var to = t.position + t.up*i + t.right*j + t.forward*castLength;
+					Debug.DrawLine(from, to, color);
+				}
+			}
+		}
+
+		void OnGUI()
+		{
+			if (debug)
+			{
+				GUILayout.Label("Player Controller");
+				if (currentItem != null)
+				{
+					GUILayout.Label("Holding a " + currentItem.name);
+				}
+			}
+		}
+
+		void OnDrawGizmos() 
+		{
+			Gizmos.color = Color.red;
+			Transform t = transform;
+			for (float i = -castHalfExtents.y; i < castHalfExtents.y; i += 0.25f)
+			{
+				Vector3 leftFrom = t.position + t.up * i - t.right * castHalfExtents.x;
+				Vector3 rightFrom = t.position + t.up * i + t.right * castHalfExtents.x;
+				Vector3 leftTo = t.position + t.up * i - t.right * castHalfExtents.x + t.forward * castLength;
+				Vector3 rightTo = t.position + t.up * i + t.right * castHalfExtents.x + t.forward * castLength;
+				Gizmos.DrawLine(leftFrom, leftTo);
+				Gizmos.DrawLine(rightFrom, rightTo);
+			}
+		}
+
+		private void db()
+		{
+			if (DetectInteractable<Ingredient>(out Ingredient ingredient))
+			{
+				Debug.Log("Detecting ingredient!: " + ingredient);
+			}
+			else if (DetectInteractable<Station>(out Station station))
+			{
+				Debug.Log("Detecting station!: " + station);
+			}
+
+		}
+	}
 }
