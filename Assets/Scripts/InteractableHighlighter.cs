3c7601c9325b2232ad3abef37d5372694f45377a
Tony Le <letony@icloud.com>
2019-05-27 05:36:21 +1000

    Begun script to highlight objects that Yoga is in front off

---
+++ Assets/Scripts/InteractableHighlighter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+namespace DirtyChefYoga
+{
+	//Continually find and highlight interactable items in front of player
+	public class InteractableHighlighter : MonoBehaviour
+	{
+		[SerializeField] float emmisionAmount = 0.5f;
+
+		Vector3 castHalfExtents;
+		float castLength;
+		LayerMask interactablesMask;
+		PlayerInteract interacter;
+
+		void Start() 
+		{
+			interacter = GetComponent<PlayerInteract>();
+			//Get cast settings
+			castHalfExtents = interacter.castHalfExtents;
+			castLength = interacter.castLength;
+			interactablesMask = interacter.interactablesMask;
+		}
+
+		void Update() 
+		{
+			var hits = Physics.OverlapBox(transform.position + transform.forward * castLength * 0.5f,
+				castHalfExtents, transform.rotation, interactablesMask);
+			
+			//Only highlight stations?
+			if (hits.Length > 0)
+			{
+				foreach (var h in hits)
+				{
+					if (!h.GetComponent<Station>())
+					{
+						var material = h.GetComponent<MeshRenderer>().material;
+						material.SetFloat("_Emission", emmisionAmount);
+					}
+				}
+			}
+		}
+	}
+}
