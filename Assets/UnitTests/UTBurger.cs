1c742ddd9e4cf9ae6a9f96eebada40899f42c311
Tony Le <letony@icloud.com>
2019-05-27 05:35:54 +1000

    Other

--- Assets/UnitTests/UTBurger.cs
+++ Assets/UnitTests/UTBurger.cs
@@ -19,42 +19,38 @@ public class UTBurger : MonoBehaviour
 	{
 		if (debug)
 		{
+			GUILayout.Label("UT Burger");
+			GUILayout.Space(5);
 
 			if (GUILayout.Button("Top Bun"))
 			{
 				var newIngredient = Instantiate(topBun, transform.position, Quaternion.identity);
-				newIngredient.GetComponent<Rigidbody>().isKinematic = true;
 				burger.AddIngredient(newIngredient.GetComponent<Ingredient>());
 			}
 
 			if (GUILayout.Button("Tomatoes"))
 			{
 				var newIngredient = Instantiate(tomatoes, transform.position, Quaternion.identity);
-				newIngredient.GetComponent<Rigidbody>().isKinematic = true;
 				burger.AddIngredient(newIngredient.GetComponent<Ingredient>());
 			}
 			if (GUILayout.Button("Lettuce"))
 			{
 				var newIngredient = Instantiate(lettuce, transform.position, Quaternion.identity);
-				newIngredient.GetComponent<Rigidbody>().isKinematic = true;
 				burger.AddIngredient(newIngredient.GetComponent<Ingredient>());
 			}
 			if (GUILayout.Button("Cheese"))
 			{
 				var newIngredient = Instantiate(cheese, transform.position, Quaternion.identity);
-				newIngredient.GetComponent<Rigidbody>().isKinematic = true;
 				burger.AddIngredient(newIngredient.GetComponent<Ingredient>());
 			}
-			if (GUILayout.Button("Cooked Patty"))
+			if (GUILayout.Button("Patty"))
 			{
 				var newIngredient = Instantiate(patty, transform.position, Quaternion.identity);
-				newIngredient.GetComponent<Rigidbody>().isKinematic = true;
 				burger.AddIngredient(newIngredient.GetComponent<Ingredient>());
 			}
 			if (GUILayout.Button("Botton Bun"))
 			{
 				var newIngredient = Instantiate(bottomBun, transform.position, Quaternion.identity);
-				newIngredient.GetComponent<Rigidbody>().isKinematic = true;
 				burger.AddIngredient(newIngredient.GetComponent<Ingredient>());
 			}
 		}
