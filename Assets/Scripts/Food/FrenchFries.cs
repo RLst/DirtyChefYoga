810c2f80a0815e2b0793c77ab66ea1096461a438
Tony Le <letony@icloud.com>
2019-05-26 19:03:26 +1000

    Added French Fries order

---
+++ Assets/Scripts/Food/FrenchFries.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+namespace DirtyChefYoga
+{
+	public class FrenchFries : Order
+	{
+		public override bool AddIngredient(Ingredient fries)
+		{
+			//French fries will always and only take one "Fries"
+			if (fries is Fries)
+			{
+				ingredients.Add(fries);
+				return true;
+			}
+
+			//Invalid ingredient
+			Debug.LogWarning("French fries can only take a single 'Fries' ingredient");
+			return false;
+		}
+	}
+}
