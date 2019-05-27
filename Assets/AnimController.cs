1094a3ed373bf9db55aad732ccede571ba43d069
Tony Le <letony@icloud.com>
2019-05-27 14:24:39 +1000

    Chef Yoga custom animations

---
+++ Assets/AnimController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+namespace DirtyChefYoga
+{
+	public class AnimController : MonoBehaviour
+	{
+
+		[SerializeField] string idleAnimTag = "Idle";
+		[SerializeField] string holdAnimTag = "Holding";
+		// [SerializeField] string pickupAnimTag = "PickingUp";
+		Animator anim;
+
+		void Awake()
+		{
+			anim = GetComponentInChildren<Animator>();
+		}
+
+		public void OnIdle()
+		{
+			anim.SetTrigger(idleAnimTag);
+		}
+		public void OnHold()
+		{
+			anim.SetTrigger(holdAnimTag);
+		}
+	}
+}
