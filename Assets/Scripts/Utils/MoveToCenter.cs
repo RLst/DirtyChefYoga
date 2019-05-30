5f29e13836d6226916de3549dc4c0886c9d11045
Tony Le <letony@icloud.com>
2019-05-30 23:02:32 +1000

    Added move to center object (so camera can track it for multiple players); Added 'Players' layer; Spawner and Conveyorbelt always need to be reconfigured

---
+++ Assets/Scripts/Utils/MoveToCenter.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+namespace DirtyChefYoga
+{
+	public class MoveToCenter : MonoBehaviour
+	{
+		[SerializeField] Transform[] objects;
+		// [SerializeField] LayerMask layer;
+		
+		Vector3 center;
+
+		// void Awake()
+		// {
+		// 	objects = FindTransformsOnLayer(layer);
+		// 	Debug.Log("Transforms" + objects.Length);
+		// }
+
+		void Update()
+		{
+			MoveToCenterAverage();
+		}
+
+		private void MoveToCenterAverage()
+		{
+			//Calculate center
+			if (objects.Length > 1)
+			{
+				var sumPosition = new Vector3();
+				foreach (var p in objects)
+					sumPosition += p.position;
+				center = sumPosition / (float)objects.Length;
+			}
+			else
+			{
+				center = objects[0].position;
+			}
+			//Move to center
+			this.transform.position = center;
+		}
+
+		Transform[] FindTransformsOnLayer(LayerMask layer)
+		{
+			var tfArray = FindObjectsOfType<Transform>();
+			var tfList = new List<Transform>();
+			for (var i = 0; i < tfArray.Length; i++)
+			{
+				if (tfArray[i].gameObject.layer == layer)
+				{
+					tfList.Add(tfArray[i]);
+				}
+			}
+			if (tfList.Count == 0) return null;
+			return tfList.ToArray();
+		}
+	}
+}
