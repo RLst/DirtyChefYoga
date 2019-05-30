5e4278acc6fdfdedc84835534f541ecfa7aff088
Tony Le <letony@icloud.com>
2019-05-31 03:09:58 +1000

    Improved and simplifed PauseManager logic

--- Assets/Scripts/Menu/PauseManager.cs
+++ Assets/Scripts/Menu/PauseManager.cs
@@ -3,70 +3,135 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class PauseManager : MonoBehaviour {
+public class PauseManager : MonoBehaviour
+{
 
-    public GameObject pausePanel;
+	// public GameObject pausePanel;
 
-    public Button resumeButton;
-    public Button restartButton;
-    public Button menuButton;
-    public Button quitButton;
+	public Button resumeButton;
+	// public Button restartButton;
+	// public Button menuButton;
+	// public Button quitButton;
 
-    bool isPaused = false;
+	bool isPaused = false;
 
-    public GameObject eventSystem;
+	[SerializeField] EventSystem eventSystem;
 
-    void Start() {
-        resumeButton.GetComponent<Button>().onClick.AddListener(PressResume);
-        restartButton.GetComponent<Button>().onClick.AddListener(PressRestart);
-        menuButton.GetComponent<Button>().onClick.AddListener(PressMenu);
-        quitButton.GetComponent<Button>().onClick.AddListener(PressQuit);
-        pausePanel.SetActive(false);
-    }
+	[SerializeField] KeyCode pauseKey = KeyCode.Escape;
+	[SerializeField] KeyCode pauseJSButton = KeyCode.JoystickButton7;
+	[SerializeField] UnityEvent OnPause, OnUnpause;
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button7)) {
-            isPaused = !isPaused;
-            if (isPaused) {
-                Time.timeScale = 0.0f;
-                pausePanel.SetActive(true);
+	void Awake()
+	{
+		eventSystem = EventSystem.current;
+	}
 
-                // Deselect previous selection and play its deselect transition:
-                if (eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().currentSelectedGameObject != null) {
-                    var previous = eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().currentSelectedGameObject.GetComponent<Selectable>();
-                    if (previous != null) {
-                        previous.OnDeselect(null);
-                        eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
-                    }
-                }
-                // Select button and play its selection transition:
-                eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(resumeButton.gameObject);
-                resumeButton.OnSelect(null);
-            } else {
-                Time.timeScale = 1.0f;
-                pausePanel.SetActive(false);
-            }
-        }
-    }
+	void Start()
+	{
+		OnPause.AddListener(Pause);
+		OnUnpause.AddListener(Resume);
 
-    void PressResume() {
-        Time.timeScale = 1.0f;
-        isPaused = false;
-        pausePanel.SetActive(false);
-    }
+		// resumeButton.GetComponent<Button>().onClick.AddListener(Resume);
+		// restartButton.GetComponent<Button>().onClick.AddListener(Restart);
+		// menuButton.GetComponent<Button>().onClick.AddListener(ReturnToMainMenu);
+		// quitButton.GetComponent<Button>().onClick.AddListener(QuitGame);
+		// pausePanel.SetActive(false);
+	}
 
-    void PressRestart() {
-        Time.timeScale = 1.0f;
-        SceneManager.LoadScene(1);
-    }
+	void Update()
+	{
+		if (Input.GetKeyDown(pauseKey) || Input.GetKeyDown(pauseJSButton))
+		{
+			isPaused = !isPaused;
 
-    void PressMenu() {
-        Time.timeScale = 1.0f;
-        SceneManager.LoadScene(0);
-    }
+			if (isPaused)
+				OnPause.Invoke();
+			else
+				OnUnpause.Invoke();
+		}
 
-    void PressQuit() {
-        Application.Quit();
-    }
+
+		// if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button7))
+		// {
+		// 	isPaused = !isPaused;
+		// 	if (isPaused)
+		// 	{
+		// 		Time.timeScale = 0.0f;
+		// 		pausePanel.SetActive(true);
+
+		// 		eventSystem.
+
+		// 		// Deselect previous selection and play its deselect transition:
+		// 		if (eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().currentSelectedGameObject != null)
+		// 		{
+		// 			var previous = eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().currentSelectedGameObject.GetComponent<Selectable>();
+		// 			if (previous != null)
+		// 			{
+		// 				previous.OnDeselect(null);
+		// 				eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
+		// 			}
+		// 		}
+		// 		// Select button and play its selection transition:
+		// 		eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(resumeButton.gameObject);
+		// 		resumeButton.OnSelect(null);
+		// 	}
+		// 	else
+		// 	{
+		// 		Time.timeScale = 1.0f;
+		// 		pausePanel.SetActive(false);
+		// 	}
+		// }
+	}
+
+	public void Pause()
+	{
+		Time.timeScale = 0f;
+		isPaused = true;
+		HandleEventSystem();
+		// pausePanel.SetActive(true);
+	}
+
+	public void Resume()
+	{
+		Time.timeScale = 1.0f;
+		isPaused = false;
+		// pausePanel.SetActive(false);
+	}
+
+	public void Restart()
+	{
+		// Time.timeScale = 1.0f;
+		SceneManager.LoadScene(1);
+	}
+
+	public void ReturnToMainMenu()
+	{
+		// Time.timeScale = 1.0f;
+		SceneManager.LoadScene(0);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	private void HandleEventSystem()
+	{
+		// Deselect previous selection and play its deselect transition:
+		if (eventSystem.currentSelectedGameObject != null)
+		{
+			var previous = eventSystem.currentSelectedGameObject.GetComponent<Selectable>();
+			if (previous != null)
+			{
+				previous.OnDeselect(null);
+				eventSystem.SetSelectedGameObject(null);
+			}
+		}
+		// Select button and play its selection transition:
+		eventSystem.SetSelectedGameObject(resumeButton.gameObject);
+		resumeButton.OnSelect(null);
+	}
 }
